01509b525d233c7d15eb4aaff4985dd398a5a8f9
mariam <mmabuladze@gmail.com>
2019-03-04 18:58:25 +0400

    textarea updated

--- skeleton/src/views/plugins/textareaField/tpl.blade.php
+++ skeleton/src/views/plugins/textareaField/tpl.blade.php
@@ -10,9 +10,7 @@ at_symbolendphp
 <div class="form-group row">
     <label class="col-md-3 col-form-label" for="text-input">{{ $properties->title }}</label>
     <div class="col-md-9">
-        <textarea rows="5" cols="50" class="form-control" name="{{ $fieldName }}" placeholder="Textarea">
-        print_start $value print_end
-        </textarea>
+        <textarea rows="5" cols="50" class="form-control" name="{{ $fieldName }}" placeholder="Textarea">print_start $value print_end</textarea>
         at_symbolif($errors->get('{{ $fieldName }}'))
             <br>
             <div class="alert alert-danger" role="alert">print_start $errors->first('{{ $fieldName }}') print_end</div>
