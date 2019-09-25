adbeb9f3ab93a9e23136a09e491cd22c1fb58bb4
mosa <gio.mosashvili1@gmail.com>
2019-09-25 19:57:26 +0400

    localization transformer

--- src/app/Http/Transformers/BaseTransformerAbstract.php
+++ src/app/Http/Transformers/BaseTransformerAbstract.php
@@ -9,24 +9,22 @@ abstract class BaseTransformerAbstract extends TransformerAbstract
 {
     // Ability to override Transformer functions
 
-    public static function transfromFilledData($data, $translatable,$moduleCfg, $translatableFields = [])
+    public static function transfromFilledData($data, $translatable, $moduleCfg, $translatableFields = [])
     {
-        $locale = App::getLocale();
-        $locales = config('translatable.locales');
-        $filledData = [];
-        $multiLang = isset($data['multilang']) && ($data['multilang'] == 'true' || $data['multilang'] == '1') ? true : false;
+        $locales = isset($data['multilang']) && ($data['multilang'] == 'true' || $data['multilang'] == '1') ? config('translatable.locales') : [App::getLocale()];
         unset($data['multilang']);
         unset($data['_token']);
         unset($data['_method']);
         unset($data['request_name_space']);
+        $filledData = $data;
+
+        foreach ($translatableFields as $key => $value) {
+            unset($filledData[$value]);
+        }
 
         if ($translatable) {
-            if (!$multiLang) {
-                $filledData = self::setDataToLocale($locale, $data);
-            } else if ($multiLang) {
-                foreach ($locales as $locale) {
-                    $filledData = self::setDataToLocale($locale, $data);
-                }
+            foreach ($locales as $locale) {
+                $filledData[$locale] = self::setDataToLocale($data, $translatableFields);
             }
         } else {
             $filledData = $data;
@@ -35,10 +33,12 @@ abstract class BaseTransformerAbstract extends TransformerAbstract
         return $filledData;
     }
 
-    public static function setDataToLocale($locale, $data)
+    public static function setDataToLocale($data, $translatableFields)
     {
+        $filledData = [];
         foreach ($data as $key => $value) {
-            $filledData[$key] = $value;
+            if (in_array($key, $translatableFields))
+                $filledData[$key] = $value;
         }
 
         return $filledData;
