50b2b111093f646a1efa24e61aaf44a4aa879f42
Mosa1 <gio.mosashvili1@gmail.com>
2021-11-15 17:07:49 +0400

    Update UsersTableSeeder.php

--- src/database/seeds/UsersTableSeeder.php
+++ src/database/seeds/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace BetterFly\Skeleton\Database\Seeds;
 use Illuminate\Database\Seeder;
 use BetterFly\Skeleton\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
